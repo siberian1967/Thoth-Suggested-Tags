47dd630768f6b072f8613eedb399fe57cd05d927
Jimmy O'Higgins <jimmy.ohiggins@gmail.com>
2010-06-24 16:57:57 -0400

    now kicks major ass, discarding tags with stop words as the first or last word, as well as tags with pure length-strength

--- rec_tags_redux.php
+++ rec_tags_redux.php
@@ -3,7 +3,6 @@
 Plugin Name: Rec Tags Redux
 Plugin URI: #
 Description: Recommends tags based on post content as well as any existing tags in the database.
-Plugin logic based on CyberNet's "Recommended Tags" plugin for WP 2.7.
 Version: 0.4
 Author: Jimmy O'Higgins
 */
@@ -48,16 +47,19 @@ function box_routine()
 			$tag_strength += $tags_db[$tag_name];
 		}
 	}
+	arsort($tags_rec);
 	//Print finals
 	$i = 0;
 	$limit = 15;
 	foreach($tags_rec as $tag_name => $tag_strength)
 	{
 		if($i++ == $limit) break;
-		echo "$indent $tag_name => $tag_strength <br/>\n";
+		$tag_length = str_word_count($tag_name);
+		if($tag_strength > $tag_length)
+		{
+			echo "$indent $tag_name => $tag_strength <br/>";
+		}
 	}
-	
-	
 	
 	//Print elements of tags_post
 	echo "Recommended tags from post<br/>\n";
@@ -120,48 +122,54 @@ function tag_list_generate_db()
 function tag_list_generate_post()
 {
 	global $post;
+	
+	$phrases = array();
+	
 	$content = $post->post_title;
 	$content .=  " ".$post->post_content;
 	$content = strip_tags($content);
 	$content = strtolower($content);
-	$content = preg_replace('/[-",.;—]/', '', $content);
-	$content_exploded = explode(" ", $content);
+	$content = preg_replace('/[\/"’“”\']/', '', $content);
 	
-	echo($content);
-	
-	$phrases = array();
+	$content_split = preg_split('/[().,!?—;:…\n]/', $content);
 	
 	$stop_words = str_replace(",", " ", " a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,don't,either,else,ever,every,for,from,get,got,had,has,have,he,her,here,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your ");
 	
-	for($phrase_length = 1; $phrase_length < 4; $phrase_length++)
+	foreach($content_split as $section)
 	{
-		for($phrase_start = 0; $phrase_start < count($content_exploded); $phrase_start++)
+		$content_exploded = explode(" ", $section);
+		for($phrase_length = 1; $phrase_length < 4; $phrase_length++)
 		{
-			$phrase = '';
-			for($word = 0; $word < $phrase_length; $word++)
+			for($phrase_start = 0; $phrase_start < count($content_exploded); $phrase_start++)
 			{
-				$position = $phrase_start+$word;
-				if(array_key_exists($position, $content_exploded))
-				{//Build phrase
-					$phrase .= $content_exploded[$position]." ";
-				}
-			}
-			
-			$phrase = trim($phrase);
-			
-			if(!(str_word_count($phrase) < $phrase_length))
-			{
-				$phrase_exploded = explode(" ", $phrase);
-				$first_word = trim($phrase_exploded[0]);
-				
-				if(!empty($phrase_exploded)
-					&& !stristr($stop_words, $first_word))
+				$phrase = '';
+				for($word = 0; $word < $phrase_length; $word++)
 				{
-					$phrase = implode(" ", $phrase_exploded);
+					$position = $phrase_start+$word;
+					if(array_key_exists($position, $content_exploded))
+					{
+						$phrase .= $content_exploded[$position]." ";
+					}
+				}
+				$phrase = trim($phrase);
+				
+				if(!(str_word_count($phrase) < $phrase_length))
+				{
+					$phrase_exploded = explode(" ", $phrase);
+					$first_word = trim($phrase_exploded[0]);
+					$count = count($phrase_exploded);
+					--$count;
+					$last_word = trim($phrase_exploded[$count]);
 					
-					//echo "adding phrase $phrase to array<br/>";
-					$phrase = trim($phrase);
-					array_push($phrases, $phrase);
+					if(!empty($phrase_exploded)
+						&& !stristr($stop_words, $first_word)
+						&& !stristr($stop_words, $last_word))
+					{
+						$phrase = implode(" ", $phrase_exploded);
+						//echo "adding phrase $phrase to array<br/>";
+						$phrase = trim($phrase);
+						array_push($phrases, $phrase);
+					}
 				}
 			}
 		}
@@ -180,13 +188,6 @@ function tag_list_generate_post()
 	
 	return $phrases;
 }
-
-/*
-function generate_tag_list()
-{//Combines tag lists from database and post data
-	
-}
-*/
 
 function print_exploded($array)
 {
