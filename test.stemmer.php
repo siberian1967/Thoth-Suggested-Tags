a3ec438e2cce254bea2a32d8c0bca6330cf8fa41
Eric Buth <eric@rcbth.com>
2010-06-25 12:02:18 -0400

    added a test

--- test.stemmer.php
+++ test.stemmer.php
@@ -1,3 +1,22 @@
 <?php
 
-require_once 'lib/class.stemmer.inc'
+require_once 'lib/class.stemmer.inc';
+
+
+# instantiate a stemmer
+
+$stemmer = new Stemmer;
+
+
+# set the word and stem it
+
+$word = "category";
+$stem = $stemmer->stem($word);
+
+
+# print the results
+
+print "The stem of $word is $stem.\n";
+
+
+?>
